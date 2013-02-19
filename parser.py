3303317117bce6b1361cefa3fdda8778d6e954a3
Cherrot Luo <cherrot+github@cherrot.com>
2013-02-20 00:07:22 +0800

    done

--- parser.py
+++ parser.py
@@ -6,40 +6,31 @@ from lxml import etree
 import urllib
 
 import csv
+import sys, getopt
 
-#class Parser:
-#    def __init__(self):
-#        self.
-
-user = ''
 records = []
-head = ['record number', 'email', 'quantity', 'price', 'sale date', 'paid date',
-        'email sent', 'title', 'detail']
 
-def parse(i, e):
+def parse(doc):
+    #userName
+    user = doc('#AreaNavigation div.mbg>a>span.mbg-nw').text()
+    
+    #selector.each(lambda i, e: pq(this).find('td#BuyerEmail').text())
+    selector = doc('#tbl_mu_active_tbl_id').find('tbody.dt-rs')
+    selector.each(parseEach)
+
+    for r in records:
+        r.update({'user': user})
+
+    return records
+
+def parseEach(i, e):
     global records
-    #content = parse1(pq(this).children('tr'))
-    #detail = parse2(pq(this).nextAll('tbody.dt-nsb').eq(0).children('tr'))
-
-    #records.append(content.update(detail))
-    content = {'record number':int(pq(this).children('tr').children('#RecordNumber').text()),
-             'email': '{}({}),'.format( #email
-                pq(this).children('tr').find('td#BuyerEmail>div>a').text(),
-                pq(this).children('tr').find('td#BuyerEmail>div>span:last').text()
-                ),
-             'quantity': int(pq(this).children('tr').children('#PurchasedQty').text()), # quantity
-             'price': pq(this).children('tr').children('#SalePrice').text(), #price
-             'sale date': pq(this).children('tr').children('#SaleDate').text(), #sale date
-             'paid date': pq(this).children('tr').children('#PaidDate').text(), #paid date
-             'email sent': pq(this).children('tr').children('#EmailSent').text(),#email sent num
-             }
-    detail = {'title': pq(this).nextAll('tbody.dt-nsb').eq(0).children('tr').find('#BuyerEmail>div>a').attr('title'),
-            'detail': pq(this).nextAll('tbody.dt-nsb').eq(0).children('tr').find('#BuyerEmail>div>div:first').text()
-            }
+    content = parseOdd(pq(this).children('tr'))
+    detail = parseEven(pq(this).nextAll('tbody.dt-nsb').eq(0).children('tr'))
     content.update(detail)
     records.append(content)
 
-def parse1(this):
+def parseOdd(this):
     return {'record number':int(this.children('#RecordNumber').text()),
              'email': '{}({}),'.format( #email
                 this.find('td#BuyerEmail>div>a').text(),
@@ -52,28 +43,45 @@ def parse1(this):
              'email sent': this.children('#EmailSent').text(),#email sent num
              }
 
-def parse2(this):
+def parseEven(this):
     return({'title': this.find('#BuyerEmail>div>a').attr('title'),
         'detail': this.find('#BuyerEmail>div>div:first').text()
         })
 
+def write2csv(filename, fields, records):
+    #export to csv file
+    dict_writer = csv.DictWriter(file(filename, 'wb'), fieldnames=fields)
+    #dict_writer.writerow(head)
+    dict_writer.writeheader()
+    #print records
+    dict_writer.writerows(records)
 
-doc = pq(filename='test/ebay.html')
-
-#userName
-user = doc('#AreaNavigation div.mbg>a>span.mbg-nw').text()
-
-#selector.each(lambda i, e: pq(this).find('td#BuyerEmail').text())
-selector = doc('#tbl_mu_active_tbl_id').find('tbody.dt-rs')
-selector.each(parse)
-
-#records2 = doc('#tbl_mu_active_tbl_id').find('tbody.dt-nsb>tr')
-#records2.each(parse2)
+def usage():
+    print \
+'''USAGE: 
+{0} {-u|--url} url file.csv     :download url to an html document and parse it to file.csv
+{0} file1.html file.csv         :parse file1.html to file.csv
+{0} -h                          :print this
+'''
 
 
-#export to csv file
-dict_writer = csv.DictWriter(file('output.csv', 'wb'), fieldnames=head)
-#dict_writer.writerow(head)
-dict_writer.writeheader()
-#print records
-dict_writer.writerows(records)
+if __name__ == '__main__':
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'hu:', ['help','url='])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(1)
+
+    for opt, arg in opts:
+        if opt in ('--help', '-h'):
+            usage()
+            sys.exit()
+        elif opt in ('--url', '-u'):
+            #TODO
+            sys.exit()
+
+    if not opts:
+        fields = ['user', 'record number', 'email', 'quantity', 'price', 'sale date', 'paid date', 'email sent', 'title', 'detail']
+
+        records = parse( pq(filename=args[0]) )
+        write2csv(args[1], fields, records)
